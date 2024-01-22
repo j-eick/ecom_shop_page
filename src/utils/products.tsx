2b9c77f43c1bf4c8e7ca7c9ed2304e407834b79a
Joschka Eickhoff <jeickhoff@posteo.de>
2024-01-22 03:03:42 +0100

    Added id and thumbnail to product.tsx

--- src/utils/products.tsx
+++ src/utils/products.tsx
@@ -1,6 +1,22 @@
-export const prodPhotos = [
-  "image-product-1.jpg",
-  "image-product-2.jpg",
-  "image-product-3.jpg",
-  "image-product-4.jpg",
+export const products = [
+  {
+    id: 1,
+    photo: "image-product-1.jpg",
+    thumbnail: "image-product-1-thumbnail.jpg",
+  },
+  {
+    id: 2,
+    photo: "image-product-2.jpg",
+    thumbnail: "image-product-2-thumbnail.jpg",
+  },
+  {
+    id: 3,
+    photo: "image-product-3.jpg",
+    thumbnail: "image-product-3-thumbnail.jpg",
+  },
+  {
+    id: 4,
+    photo: "image-product-4.jpg",
+    thumbnail: "image-product-4-thumbnail.jpg",
+  },
 ];
